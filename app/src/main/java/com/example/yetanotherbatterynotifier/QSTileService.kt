06bbfc16dc76efe2bf19c3dda4a3f08809a07459
maary <24504742+Steve-Mr@users.noreply.github.com>
2022-07-29 21:14:23 +0800

    将注册通知渠道移到 tile service 中

--- app/src/main/java/com/example/yetanotherbatterynotifier/QSTileService.kt
+++ app/src/main/java/com/example/yetanotherbatterynotifier/QSTileService.kt
@@ -1,11 +1,13 @@
 package com.example.yetanotherbatterynotifier
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 
 class QSTileService: TileService() {
-    var isClicked = false
 
     override fun onClick() {
         super.onClick()
@@ -14,14 +16,37 @@ class QSTileService: TileService() {
         val intent = Intent(this, ForegroundService::class.java)
 
         if (!ForegroundService.isForegroundServiceRunning()){
+            createNotificationChannel(
+                resources.getString(R.string.default_channel),
+                resources.getString(R.string.default_channel_description)
+            )
+            createNotificationChannel(
+                resources.getString(R.string.channel_notify),
+                resources.getString(R.string.channel_notify_description)
+            )
+            
             applicationContext.startForegroundService(intent)
             tile.state = Tile.STATE_ACTIVE
-            isClicked = true
+
+
+
         }else{
             applicationContext.stopService(intent)
             tile.state = Tile.STATE_INACTIVE
-            isClicked = false
         }
         tile.updateTile()
+    }
+
+    private fun createNotificationChannel(name:String, descriptionText: String) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(name, name, importance).apply {
+            description = descriptionText
+        }
+        // Register the channel with the system
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
