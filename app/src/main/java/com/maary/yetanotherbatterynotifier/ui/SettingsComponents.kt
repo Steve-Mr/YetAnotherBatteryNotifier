05cba0f0673091c7ad0bdf4015a20b65daeb9318
Maary <24504742+Steve-Mr@users.noreply.github.com>
2024-04-30 20:27:20 +0800

    Better SwitchRow

--- app/src/main/java/com/maary/yetanotherbatterynotifier/ui/SettingsComponents.kt
+++ app/src/main/java/com/maary/yetanotherbatterynotifier/ui/SettingsComponents.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -49,6 +50,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
@@ -250,6 +252,11 @@ class SettingsComponents {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
+                .pointerInput(Unit) {
+                    detectTapGestures {
+                        onCheckedChange(!state) // 当点击 SwitchRow 时触发点击事件
+                    }
+                }
                 .padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
@@ -274,6 +281,7 @@ class SettingsComponents {
             if (state) {
                 TextContent(
                     modifier = Modifier
+                        .fillMaxWidth()
                         .clickable { onNotificationSettingsClicked() }
                         .padding(start = 32.dp, top = 8.dp, end = 32.dp, bottom = 8.dp),
                     title = stringResource(id = R.string.notification_settings),
